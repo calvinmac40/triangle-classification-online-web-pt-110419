489ae15238297ed0249d73caaa6d19fdbdb8667d
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-20 02:55:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,10 +15,10 @@ class Triangle
       elsif 
       (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0) 
       raise TriangleError
-    elsif
-      (@side1 == @side2) && (@side2 == @side3)
-      :equilateral
     else
+      if (@side1 == @side2) && (@side2 == @side3)
+      :equilateral
+    elsif
       (@side1 == @side2) ||  (@side2 == @side3) || (@side1 == @side3)
       :isoseceles
     
